e4b0cbe3b096013ade2d957e179ebf072c384034
Ravi S. Rāmphal <rramphal@gmail.com>
2021-03-14 21:31:10 -0500

    added toggleable list

--- README.md
+++ README.md
@@ -2,9 +2,27 @@
 
 ## Lists
 
-Type | URL
----- | ---
-All  | https://raw.githubusercontent.com/rramphal/hosts/master/lists/blacklist
+Name                    | URL
+----------------------- | ---
+**`blacklist`**         | **https://raw.githubusercontent.com/rramphal/hosts/master/lists/blacklist**
+`toggleable`            | https://raw.githubusercontent.com/rramphal/hosts/master/lists/toggleable
+`all` (not recommended) | https://raw.githubusercontent.com/rramphal/hosts/master/lists/all
+
+### What is the `toggleable` list?
+
+There are some domains that should not be blocked at the network level, but rather at the discretion of the user at the client level.
+Blocking them at the network could break key functionality of some sites.
+Instead, they could be blocked using a browser extension (for example, as a custom filter list within [uBlock Origin](https://github.com/gorhill/uBlock)).
+
+#### Case Study
+
+An example of a domain that is toggleable is `m.stripe.com`.
+On April 21, 2020, [Michael Lynch](https://mtlynch.io/stripe-recording-its-customers/) reported that Stripe was silently recording user behavior on websites.
+Categorically blocking Stripe would break payment functionality across many legitimate sites.
+Stripe uses this tracking to drive the machine learning system that contributes to fraud prevention.
+That said, this is only important when users are going to actually pay using Stripe.
+If users are just browsing, there is no reason anyone needs to be tracking them.
+Since Stripe has no way of knowing what users' intentions are, it's best left up to the users themselves to enable Stripe when relevant.
 
 ## Updating
 
--- generate.js
+++ generate.js
@@ -1,17 +1,40 @@
 const path = require('path');
 const fs   = require('fs');
 
-const INPUT_DIRECTORY = path.join(__dirname, 'data');
-const OUTPUT_FILEPATH = path.join(__dirname, 'lists', 'blacklist');
+const INPUT_DIRECTORY  = path.join(__dirname, 'data');
+const OUTPUT_DIRECTORY = path.join(__dirname, 'lists');
 
-const PREAMBLE = `
-# Source: https://raw.githubusercontent.com/rramphal/hosts/master/lists/blacklist
-
-`;
-
-const POSTAMBLE = `
-# Last updated: ${getDate()}
-`;
+const LISTS = [
+  {
+    name   : 'all',
+    filter : (filenames) => {
+      return filenames
+        .filter((filename) => !filename.startsWith('.')) // no hidden files
+        .filter((filename) => filename.endsWith('.txt')) // only `.txt` files
+      ;
+    },
+  },
+  {
+    name   : 'blacklist',
+    filter : (filenames) => {
+      return filenames
+        .filter((filename) => !filename.startsWith('.'))        // no hidden files
+        .filter((filename) => filename.endsWith('.txt'))        // only `.txt` files
+        .filter((filename) => !filename.includes('toggleable')) // no toggleable files
+      ;
+    },
+  },
+  {
+    name   : 'toggleable',
+    filter : (filenames) => {
+      return filenames
+        .filter((filename) => filename.endsWith('.txt'))       // only `.txt` files
+        .filter((filename) => !filename.startsWith('.'))       // no hidden files
+        .filter((filename) => filename.includes('toggleable')) // only toggleable files
+      ;
+    },
+  },
+];
 
 function getDate () {
   return new Date().toLocaleString('sv-SE', {
@@ -21,7 +44,7 @@ function getDate () {
     hour     : '2-digit',
     minute   : '2-digit',
     hour12   : false,
-    timeZone : 'America/Chicago'
+    timeZone : 'America/Chicago',
   });
 }
 
@@ -36,37 +59,44 @@ function pad (input) {
   return `[${input}]`.padEnd(12);
 }
 
+console.log();
 
-const all_files = fs.readdirSync(INPUT_DIRECTORY);
+LISTS.forEach(({ name, filter }) => {
+  const preamble  = `\n# Source: https://raw.githubusercontent.com/rramphal/hosts/master/lists/${name}\n\n`;
+  const postamble = `\n# Last updated: ${getDate()}\n`;
 
-const files = all_files
-  .filter((filename) => filename.endsWith('.txt')) // only `.txt` files
-  .filter((filename) => !filename.startsWith('.')) // no hidden files
-;
+  const allFiles = fs.readdirSync(INPUT_DIRECTORY);
+  const files    = filter(allFiles);
 
-const output = [];
+  const output = [];
 
-files.forEach((fileName) => {
-  const filePath = path.join(INPUT_DIRECTORY, fileName);
+  console.log(pad('LIST'), name);
 
-  console.log(pad('PROCESSING'), filePath);
+  files.forEach((fileName) => {
+    const filePath = path.join(INPUT_DIRECTORY, fileName);
 
-  const data  = fs.readFileSync(filePath, 'utf8').toString();
-  const lines = data.split('\n');
+    console.log(pad('PROCESSING'), filePath);
 
-  lines.forEach((line) => {
-    const trimmedLine = line.trim();
+    const data  = fs.readFileSync(filePath, 'utf8').toString();
+    const lines = data.split('\n');
 
-    if (trimmedLine.startsWith('#') || trimmedLine.length === 0) {
-      output.push(trimmedLine);
-    } else {
-      output.push(formatDomain(trimmedLine));
-    }
+    lines.forEach((line) => {
+      const trimmedLine = line.trim();
+
+      if (trimmedLine.startsWith('#') || trimmedLine.length === 0) {
+        output.push(trimmedLine);
+      } else {
+        output.push(formatDomain(trimmedLine));
+      }
+    });
   });
+
+  const outputFilepath = path.join(OUTPUT_DIRECTORY, name);
+
+  console.log(pad('WRITING'), outputFilepath);
+
+  fs.writeFileSync(outputFilepath, preamble + output.join('\n') + postamble);
+
+  console.log(pad('DONE'));
+  console.log();
 });
-
-console.log(pad('WRITING'), OUTPUT_FILEPATH);
-
-fs.writeFileSync(OUTPUT_FILEPATH, PREAMBLE + output.join('\n') + POSTAMBLE);
-
-console.log(pad('DONE'));
